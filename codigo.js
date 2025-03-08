360e36681f3725fa27431487f192b36537781db9
marte687 <155115599+marte687@users.noreply.github.com>
2025-03-08 17:15:18 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,27 @@
+
+function carregar(){
+    var foto = document.getElementById('imagem')
+    var msg = document.getElementById('msg')
+    var aviso = document.querySelector('div#aviso')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora}h`
+    
+    if (hora <= 12 && hora >= 6){
+        foto.src = 'dia.jpg'
+        aviso.innerHTML += 'Bom Dia!'
+    }
+
+   else if(hora>=13 && hora <18){
+         foto.src = 'tarde.jpg'
+         document.body.style.background = 'orange'
+         aviso.innerHTML += 'Boa Tarde!'
+    }
+
+    else{
+        foto.src = 'noite2.png'
+        document.body.style.background = '#272727'
+        aviso.innerHTML += 'Boa Noite!'
+        favicon.href = 'Lua.ico'
+    }
+}
